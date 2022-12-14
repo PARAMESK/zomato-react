20e1699381ab1ac9b5aee5233675b2113a5b7657
PARAMESK <parameshk231@gmail.com>
2022-12-14 22:00:37 +0530

    header service card added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Restaurant from './Pages/Restaurant/Restaurant'
+import Checkout from './Pages/Checkout/Checkout'
+import Home from './Pages/Home/Home';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path = '/' element = {<Home/>}/>
+          <Route path = '/restaurant' element = {<Restaurant/>}/>
+          <Route path = '/checkout' element = {<Checkout/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+
+// routes
+// 1. home page 
+// 2. restaurant
+// 3. cart 
+// 4. 
